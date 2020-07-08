ca45bf108ace1408eb769ce12b9ecdbc7b648c91
Izzu Zantya Fawwas <49353967+izzuzantyaf@users.noreply.github.com>
2020-07-08 18:54:46 +0700

    add production build

---
+++ build/precache-manifest.60ac5ddc13010189099cea6f69973819.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "055cc582e16ede101506da7d63f9d38c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60cc34e5924b03c3c707",
+    "url": "/static/css/main.3e3f4aba.chunk.css"
+  },
+  {
+    "revision": "95badf73a6ab1d7f601c",
+    "url": "/static/js/2.d8a2ffcd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d8a2ffcd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cc34e5924b03c3c707",
+    "url": "/static/js/main.9f1e66b9.chunk.js"
+  },
+  {
+    "revision": "cfba3f5466aec1aa69a7",
+    "url": "/static/js/runtime-main.82aefd60.js"
+  },
+  {
+    "revision": "e2467230d4b92f01047ecb15c52eed12",
+    "url": "/static/media/fisdas-logo-min.e2467230.png"
+  }
+]);
