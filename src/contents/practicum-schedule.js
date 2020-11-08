64121638d94fc87deb90823225e85a97b6dfc952
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-11-08 09:41:17 +0700

    practicum schedule revision

--- src/contents/practicum-schedule.js
+++ src/contents/practicum-schedule.js
@@ -1,12 +1,12 @@
 export const practicumSchedule = [
     {
         faculty: 'fte',
-        content: ['PAP', 'GMB', 'Presentasi', 'GJB', 'Presentasi', 'SGH', 'Presentasi', 'GLB', 'Presentasi', 'RGB', 'Presentasi'
+        content: ['PAP', 'GMB', 'Presentasi', 'GJB', 'Presentasi', 'SGH', 'GLB', 'Presentasi', 'RGB', 'Presentasi'
         ]
     },
     {
         faculty: 'fri',
-        content: ['PAP', 'GMB', 'Presentasi', 'GJB', 'Presentasi', 'PBL', 'Presentasi', 'GLB', 'Presentasi', 'IM', 'Presentasi'
+        content: ['PAP', 'GMB', 'Presentasi', 'GJB', 'Presentasi', 'PBL', 'GLB', 'Presentasi', 'IM', 'Presentasi'
         ]
     }
 ]
