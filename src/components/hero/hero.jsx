fbeba46f0d43715319ca02e8cd81150148138116
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-02-20 12:05:07 +0700

    add recruitation poster

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from 'react'
 import './hero.scss'
 
+import oprecPoster from '../../assets/img/poster_rekruitasi_21.jpg'
+
 // level 0 component
 export default function Hero() {
 
@@ -14,6 +16,7 @@ export default function Hero() {
                 <div className="main-title">Laboratorium Fisika Dasar <br></br> Universitas Telkom</div>
                 <div className="desc">The official website of<br />Tel-U Physics Laboratory</div>
             </div>
+            <img src={oprecPoster} alt="rekruitasi" style={{ maxWidth: 500, borderRadius: '0.8em' }} />
         </section>
     )
 }
