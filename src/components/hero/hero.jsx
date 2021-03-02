e4bd6ac79e151ef2116a09f5753810e49be00b4d
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-03-02 12:15:48 +0700

    remove open recruitment poster

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import './hero.scss'
 
-import oprecPoster from '../../assets/img/poster_rekruitasi_21.jpg'
+// import oprecPoster from '../../assets/img/poster_rekruitasi_21.jpg'
 
 // level 0 component
 export default function Hero() {
@@ -16,7 +16,7 @@ export default function Hero() {
                 <div className="main-title">Laboratorium Fisika Dasar <br></br> Universitas Telkom</div>
                 <div className="desc">The official website of<br />Tel-U Physics Laboratory</div>
             </div>
-            <img src={oprecPoster} alt="rekruitasi" style={{ maxWidth: 500, borderRadius: '0.8em' }} />
+            {/* <img src={oprecPoster} alt="rekruitasi" style={{ maxWidth: 500, borderRadius: '0.8em' }} /> */}
         </section>
     )
 }
