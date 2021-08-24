c4da57dde0c652caec0a1b3e2b7cc87500078780
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-08-24 11:44:50 +0700

    implement tailwindcss in navbar

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import './navbar.scss'
 
 import fisdasLogo from '../../assets/img/fisdas-logo-min.png'
 import menuArr from '../../contents/menu'
@@ -12,49 +11,54 @@ function Logo() {
 
     return (
         <div className="logo">
-            <Link to="/" style={{ textDecoration: 'none' }}>
-                <img src={fisdasLogo} alt="fisdas-logo" />
+            <Link to="/">
+                <img src={fisdasLogo} alt="fisdas-logo" width="50" />
             </Link>
         </div>
     )
 }
 
-function Navigation() {
-
-    const menuList = menuArr
-        .map((menu, index) =>
-            <Link
-                key={index}
-                to={menu.link}
-                style={{ textDecoration: 'none', color: 'black' }}
-                className="menu-list-item">
-                {menu.name}
-            </Link>
-        )
+function DropDownMenu() {
 
     useEffect(() => {
         const hamburgerIcon = document.querySelector(".hamburger-icon")
-        const dropdownMenu = document.querySelector(".menu-list")
-        hamburgerIcon.addEventListener('mouseenter', () => {
-            dropdownMenu.classList.add('show')
-        })
+        const dropdownMenu = document.querySelector(".dropdown")
+        const dropDownOverlay = document.querySelector(".dropdown-overlay")
         hamburgerIcon.addEventListener('click', () => {
-            dropdownMenu.classList.toggle('show')
+            if (dropdownMenu.classList.contains('hidden')) {
+                dropdownMenu.classList.replace('hidden', 'flex')
+                dropDownOverlay.classList.add('fixed')
+            }
+            else {
+                dropdownMenu.classList.replace('flex', 'hidden')
+                dropDownOverlay.classList.remove('fixed')
+            }
         })
-        dropdownMenu.addEventListener('mouseleave', () => {
-            dropdownMenu.classList.remove('show')
+        dropDownOverlay.addEventListener('click', (e) => {
+            if (e.target !== dropdownMenu) {
+                dropdownMenu.classList.replace('flex', 'hidden')
+                dropDownOverlay.classList.remove('fixed')
+            }
         })
         return () => { }
     }, [])
 
     return (
         <div className="navigation">
-            <FontAwesomeIcon
-                icon="bars"
-                className="hamburger-icon"
-            />
-            <div className="menu-list">
-                {menuList}
+            <div className="dropdown-overlay inset-0 bg-black bg-opacity-30">
+                <div className="dropdown absolute top-14 right-4 hidden flex-col bg-white rounded-xl px-2 py-3 border">
+                    {menuArr.map((menu, index) =>
+                        <Link
+                            key={index}
+                            to={menu.link}
+                            className="dropdown-item rounded-lg hover:bg-gray-100 p-2">
+                            {menu.name}
+                        </Link>
+                    )}
+                </div>
+            </div>
+            <div className="hamburger-icon cursor-pointer rounded-md text-blue-600 text-lg hover:bg-blue-100 px-2 py-1">
+                <FontAwesomeIcon icon="bars" />
             </div>
         </div>
     )
@@ -64,10 +68,10 @@ function Navigation() {
 export default function Navbar() {
 
     return (
-        <section className="navbar">
-            <div className="container">
+        <section className="navbar bg-white top-0">
+            <div className="container relative flex items-center mx-auto justify-between p-4">
                 <Logo />
-                <Navigation />
+                <DropDownMenu />
             </div>
         </section>
     )
