3d4470a69105dee82f33d46234fc60740b3a26c1
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-09-12 14:34:19 +0700

    add assistant contacting feature

--- src/pages/assistant/index.jsx
+++ src/pages/assistant/index.jsx
@@ -3,7 +3,6 @@ import ContentLayout from '../../layouts/content'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { getData } from '../../lib/get-data'
 
-// level 2 component
 function AssistantCard(props) {
     const [feedbackTime] = useState(false)
     const { name, code, line_id, feedback_link } = props.data
@@ -12,12 +11,20 @@ function AssistantCard(props) {
         <div className="assistant-card flex flex-col gap-1 bg-white p-4 rounded-xl hover:shadow-lg transition-shadow duration-300">
             <div className="assistant-code font-bold text-2xl text-blue-800">{code}</div>
             <div className="assitants-name flex-grow">{name}</div>
-            <div className="line flex justify-between items-center">
-                <div className="icon text-green-600 text-2xl">
-                    <FontAwesomeIcon icon={['fab', 'line']} />
-                </div>
-                <div className="line-id">{line_id ?? '-'}</div>
-            </div>
+            {
+                line_id ?
+                    <a href={`https://line.me/R/ti/p/${line_id}`} target="_blank" rel="noopener noreferrer" className="border border-green-500 text-green-500 p-2 rounded-lg text-xs">
+                        <div className="flex gap-2 items-center">
+                            <FontAwesomeIcon icon={['fab', 'line']} className="text-2xl" />
+                            Contact me
+                        </div>
+                    </a>
+                    :
+                    <div className="flex gap-2 items-center bg-gray-100 text-gray-500 p-2 rounded-lg text-xs">
+                        <FontAwesomeIcon icon={['fab', 'line']} className="text-2xl" />
+                        No Line ID
+                    </div>
+            }
             {feedbackTime ?
                 <a
                     href={feedback_link}
@@ -29,7 +36,6 @@ function AssistantCard(props) {
     )
 }
 
-// level 0 component
 export default function Assistant() {
     const [assistants, setAssistants] = useState([])
 
