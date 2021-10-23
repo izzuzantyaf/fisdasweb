51adde429169eb313fc290eb2aed92d918fa88d2
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-10-23 19:40:17 +0700

    add experimental banner when not in production env

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import React from "react"
 import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom"
 
+// ambil icon yang diperlukan saja
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faFacebook, faTwitter, faInstagram, faLine, faDiscord, faTiktok, faLinkedin, faTelegramPlane, faYoutube } from "@fortawesome/free-brands-svg-icons"
+import {
+  faCoffee, faListOl, faBook, faPlay, faUsers, faCalendarMinus, faSitemap, faGlobe, faEye, faDownload, faTasks, faBalanceScale, faSortNumericUpAlt, faGripLinesVertical, faCircleNotch, faInfinity, faSatelliteDish, faWaveSquare, faMagnet, faBolt, faChevronCircleUp, faFile, faChartLine, faCogs, faBars, faCalculator, faPlug, faDrawPolygon, faParachuteBox
+} from "@fortawesome/free-solid-svg-icons"
+
 import AppLayout from "./layouts/app"
 
 import Home from "./pages/home"
@@ -15,33 +22,32 @@ import Assistant from "./pages/assistant"
 import PracticumSchedule from "./pages/practicum-schedule"
 import Organigram from "./pages/organigram"
 import NotFoundPage from "./pages/not-found-page"
+import ExperimentalBanner from "./components/experimental-banner"
 
-import { library } from "@fortawesome/fontawesome-svg-core"
-import { faFacebook, faTwitter, faInstagram, faLine, faDiscord, faTiktok, faLinkedin, faTelegramPlane, faYoutube } from "@fortawesome/free-brands-svg-icons"
-import {
-  faCoffee, faListOl, faBook, faPlay, faUsers, faCalendarMinus, faSitemap, faGlobe, faEye, faDownload, faTasks, faBalanceScale, faSortNumericUpAlt, faGripLinesVertical, faCircleNotch, faInfinity, faSatelliteDish, faWaveSquare, faMagnet, faBolt, faChevronCircleUp, faFile, faChartLine, faCogs, faBars, faCalculator, faPlug, faDrawPolygon, faParachuteBox
-} from "@fortawesome/free-solid-svg-icons"
-
+// jangan lupa register icon yang udah diambil diatas, biar bisa dipake
 library.add(faFacebook, faTwitter, faInstagram, faLine, faDiscord, faTiktok, faLinkedin, faTelegramPlane, faYoutube, faCoffee, faListOl, faBook, faPlay, faUsers, faCalendarMinus, faSitemap, faGlobe, faEye, faDownload, faTasks, faBalanceScale, faSortNumericUpAlt, faGripLinesVertical, faCircleNotch, faInfinity, faSatelliteDish, faWaveSquare, faMagnet, faBolt, faChevronCircleUp, faFile, faChartLine, faCogs, faBars, faCalculator, faPlug, faDrawPolygon, faParachuteBox)
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" render={() => <Home />} />
-        <Route path="/code-of-conduct" render={() => <AppLayout content={<CodeOfConduct />} />} />
-        <Route path="/handout" render={() => <AppLayout content={<Handout />} />} />
-        <Route path="/preliminary-test" render={() => <AppLayout content={<PreliminaryTest />} />} />
-        <Route path="/practicum-video" render={() => <AppLayout content={<PracticumVideo />} />} />
-        <Route path="/regression" render={() => <AppLayout content={<Regression />} />} />
-        <Route path="/simulator" render={() => <AppLayout content={<PracticumSimulator />} />} />
-        <Route path="/journal-cover" render={() => <AppLayout content={<JournalCover />} />} />
-        <Route path="/assistant" render={() => <AppLayout content={<Assistant />} />} />
-        <Route path="/schedule" render={() => <AppLayout content={<PracticumSchedule />} />} />
-        <Route path="/organigram" render={() => <AppLayout content={<Organigram />} />} />
-        <Route path="/404" render={() => <NotFoundPage />} />
-        <Redirect to="/404" />
-      </Switch>
-    </Router>
+    <>
+      {process.env.REACT_APP_APP_ENV !== 'production' ? <ExperimentalBanner /> : null}
+      <Router>
+        <Switch>
+          <Route exact path="/" render={() => <Home />} />
+          <Route path="/code-of-conduct" render={() => <AppLayout content={<CodeOfConduct />} />} />
+          <Route path="/handout" render={() => <AppLayout content={<Handout />} />} />
+          <Route path="/preliminary-test" render={() => <AppLayout content={<PreliminaryTest />} />} />
+          <Route path="/practicum-video" render={() => <AppLayout content={<PracticumVideo />} />} />
+          <Route path="/regression" render={() => <AppLayout content={<Regression />} />} />
+          <Route path="/simulator" render={() => <AppLayout content={<PracticumSimulator />} />} />
+          <Route path="/journal-cover" render={() => <AppLayout content={<JournalCover />} />} />
+          <Route path="/assistant" render={() => <AppLayout content={<Assistant />} />} />
+          <Route path="/schedule" render={() => <AppLayout content={<PracticumSchedule />} />} />
+          <Route path="/organigram" render={() => <AppLayout content={<Organigram />} />} />
+          <Route path="/404" render={() => <NotFoundPage />} />
+          <Redirect to="/404" />
+        </Switch>
+      </Router>
+    </>
   )
 }
---
+++ src/components/experimental-banner/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ExperimentalBanner() {
+  return (
+    <div className="experimental-banner text-center p-1 bg-yellow-300">You're accessing experimental channel</div>
+  )
+}
