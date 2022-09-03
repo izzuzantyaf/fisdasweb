6e56f9efdd1527a5342e9f2663e5c0e059cb2f7f
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-09-03 18:49:12 +0700

    refactor: add head and title

--- pages/schedules.tsx
+++ pages/schedules.tsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from "react"
 import MenuPageLayout from "../layouts/menu-page.layout"
-import DocumentFrame from "../components/document-frame.comp"
-import { getData } from "../lib/get-data"
 import { Schedule } from "../core/types/schedule.type"
 import { scheduleService } from "../core/services/schedule.service"
 import { Box, Heading, Text } from "@chakra-ui/react"
+import Head from "next/head"
 
 export default function PracticumSchedulePage() {
   const [schedulesState, setSchedulesState] = useState<Schedule[]>()
@@ -23,26 +22,31 @@ export default function PracticumSchedulePage() {
   }, [])
 
   return (
-    <MenuPageLayout pageTitle="Jadwal Praktikum">
-      <div className="grid sm:grid-cols-2 gap-6">
-        {schedulesState?.map(({ faculty, embedURL }, index) => (
-          <Box key={index}>
-            <Heading fontWeight="medium" fontSize={24}>{`Jadwal ${
-              faculty ? faculty.toUpperCase() : "Kelas"
-            }`}</Heading>
-            <iframe
-              className="bg-gray-100 mt-4"
-              title={`Jadwal ${faculty ? faculty.toUpperCase() : "Kelas"}`}
-              src={embedURL}
-              width="100%"
-              style={{
-                borderRadius: "12px",
-              }}
-              height={360}
-            ></iframe>
-          </Box>
-        ))}
-      </div>
-    </MenuPageLayout>
+    <>
+      <Head>
+        <title>Jadwal Praktikum | Lab Fisika Dasar Universitas Telkom</title>
+      </Head>
+      <MenuPageLayout pageTitle="Jadwal Praktikum">
+        <div className="grid sm:grid-cols-2 gap-6">
+          {schedulesState?.map(({ faculty, embedURL }, index) => (
+            <Box key={index}>
+              <Heading fontWeight="medium" fontSize={24}>{`Jadwal ${
+                faculty ? faculty.toUpperCase() : "Kelas"
+              }`}</Heading>
+              <iframe
+                className="bg-gray-100 mt-4"
+                title={`Jadwal ${faculty ? faculty.toUpperCase() : "Kelas"}`}
+                src={embedURL}
+                width="100%"
+                style={{
+                  borderRadius: "12px",
+                }}
+                height={360}
+              ></iframe>
+            </Box>
+          ))}
+        </div>
+      </MenuPageLayout>
+    </>
   )
 }
