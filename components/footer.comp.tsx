582277297253d693c539dbea4491366be0349484
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2022-10-12 09:11:25 +0700

    update: update some style and spacing

--- components/footer.comp.tsx
+++ components/footer.comp.tsx
@@ -1,28 +1,20 @@
-import { useState, useEffect } from "react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { getData } from "../lib/get-data"
 import { menu } from "../core/lib/constants"
 import Link from "next/link"
 import { Box, Container } from "@chakra-ui/react"
 
 export default function Footer() {
-  const [socialMedia, setSocialMedia] = useState()
-
-  const getSocialMedia = async () => {
-    const data = await getData("social-media")
-    setSocialMedia(data)
-  }
-
-  useEffect(() => {
-    getSocialMedia()
-  }, [])
-
   return (
-    <Box as="footer" className="footer text-gray-600" paddingY="32px">
+    <Box
+      as="footer"
+      className="footer text-gray-600"
+      marginTop="32px"
+      paddingY="32px"
+    >
       <Container maxWidth="container.lg">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12">
-          <div className="identity col-span-full flex flex-col gap-4">
-            <p className="title text-2xl font-bold">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="identity col-span-full flex flex-col gap-8">
+            <p className="title text-2xl font-extrabold">
               Laboratorium Fisika Dasar<br></br>Universitas Telkom
             </p>
             {/* <div className="socmed text-2xl flex gap-4">
@@ -44,13 +36,14 @@ export default function Footer() {
           <div className="headquarters">
             <p className="title font-bold">Our headquarters</p>
             <p className="address mt-4">
-              TULT 10.103-10.105 JL. Telekomunikasi Trs. Buah Batu, Bandung, 40257
+              TULT 10.103-10.105 JL. Telekomunikasi Trs. Buah Batu, Bandung,
+              40257
             </p>
           </div>
 
           <div className="explore">
             <p className="title font-bold">Explore</p>
-            <div className="footer-menu-list flex flex-wrap mt-4 gap-4">
+            <div className="footer-menu-list flex flex-wrap mt-4 gap-2">
               {menu.map((menu, index) => (
                 <Link
                   key={index}
@@ -64,7 +57,7 @@ export default function Footer() {
           </div>
 
           <div
-            className="back-to-top-icon col-span-full text-center text-2xl cursor-pointer"
+            className="back-to-top-icon col-span-full text-center text-2xl cursor-pointer mt-8"
             onClick={() => window.scrollTo(0, 0)}
           >
             <FontAwesomeIcon icon="chevron-circle-up" />
