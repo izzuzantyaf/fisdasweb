316544cf06c8e08c8779cc66c36418fca0aa754c
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2023-02-03 14:50:45 +0700

    update: change script injection using Script next

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,6 +12,7 @@ import { library } from "@fortawesome/fontawesome-svg-core"
 import { config } from "@fortawesome/fontawesome-svg-core" //* untuk mengatasi bug ukuran icon jadi sangat besar di Next.js
 import "@fortawesome/fontawesome-svg-core/styles.css" //* untuk mengatasi bug ukuran icon jadi sangat besar di Next.js
 import Head from "next/head"
+import Script from "next/script"
 
 // prettier-ignore
 library.add(faFacebook, faTwitter, faInstagram, faLine, faDiscord, faTiktok, faLinkedin, faTelegramPlane, faYoutube, faCoffee, faListOl, faBook, faPlay, faUsers, faCalendarMinus, faSitemap, faGlobe, faEye, faDownload, faTasks, faBalanceScale, faSortNumericUpAlt, faGripLinesVertical, faCircleNotch, faInfinity, faSatelliteDish, faWaveSquare, faMagnet, faBolt, faChevronCircleUp, faFile, faChartLine, faCogs, faBars, faCalculator, faPlug, faDrawPolygon, faParachuteBox, faGamepad, faQuestion)
@@ -34,13 +35,14 @@ const customizedTheme = extendTheme({
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <script
+      {/* <Head> */}
+      {/* <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676144620857119"
           crossOrigin="anonymous"
-        ></script>
-      </Head>
+        ></script> */}
+      {/* </Head> */}
+      <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676144620857119" />
       <ChakraProvider theme={customizedTheme}>
         <Component {...pageProps} />
       </ChakraProvider>
