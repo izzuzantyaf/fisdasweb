c164ec04158c5b3642fc3c0b33ca588e159e00d0
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2023-06-15 01:46:37 +0700

    feat: add article cover image

--- next.config.js
+++ next.config.js
@@ -9,7 +9,18 @@ const nextConfig = {
   },
   typescript: {
     ignoreBuildErrors: true
-  }
+  },
+  images: {
+    domains: ['images.unsplash.com'],
+    // remotePatterns: [
+    //   {
+    //     protocol: 'https',
+    //     hostname: 'images.unsplash.com',
+    //     port: '',
+    //     pathname: '/**',
+    //   },
+    // ],
+  },
 }
 
 module.exports = nextConfig
