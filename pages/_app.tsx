1828676e31b3198ba81b371a6c1f83659b89be4c
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2024-02-07 15:13:45 +0700

    chore: disable google ads

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,6 @@ import {
 import { library } from "@fortawesome/fontawesome-svg-core"
 import { config } from "@fortawesome/fontawesome-svg-core" //* untuk mengatasi bug ukuran icon jadi sangat besar di Next.js
 import "@fortawesome/fontawesome-svg-core/styles.css" //* untuk mengatasi bug ukuran icon jadi sangat besar di Next.js
-import Head from "next/head"
-import Script from "next/script"
 import { Analytics } from "@vercel/analytics/react"
 import formbricks from "@formbricks/js"
 import { useEffect } from "react"
@@ -69,14 +67,6 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <Head>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676144620857119"
-          crossOrigin="anonymous"
-        ></script>
-      </Head>
-      {/* <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9676144620857119" /> */}
       <ChakraProvider theme={customizedTheme}>
         <Component {...pageProps} />
       </ChakraProvider>
